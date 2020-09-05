00cbc0344cd72bee24bf369a1dac4674e2393b21
Kabra <eva.kabra@accenture.com>
2020-09-05 15:44:35 +0530

    init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=7ff99ccbd93409150543fd65d8572d35&query=' + latitude + ',' + longitude;
+    request({ url, json: true }, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather service', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degress out')
+        }
+    })
+}
+
+module.exports = forecast
